9615e9d04f8c91427a383fad714c8a50f5d3049f
Kuro0001 <antaa200@gmail.com>
2021-06-09 12:34:32 +0300

    результат сотрудников. сделано.

---
+++ Lab1/Controllers/DetailControllers/DetailEmployeeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Lab1.Models;
+using Lab1.Models.DetailModels;
+using Lab1.Models.Entities;
+
+namespace Lab1.Controllers.DetailControllers
+{
+    public class DetailEmployeeController : Controller
+    {
+        private TouristAgencyDbEntities db = new TouristAgencyDbEntities();
+        private List<DetailEmployee> allTables = new List<DetailEmployee>();
+
+        // GET: ToSelectInfo
+        public ActionResult EmployeeDetail()
+        {
+            List<Employee> list_emp = db.Employees.ToList();
+            foreach (var item in list_emp)
+            {
+                List<Voucher> list_vouch = db.Vouchers.Where(e => e.EmployeeId == item.ID).ToList();
+                double money = 0;
+                foreach(var item_vouch in list_vouch)
+                {
+                    money += Convert.ToDouble(item_vouch.Price);
+                }
+                DetailEmployee result = new DetailEmployee();
+
+                result.bio = item.Surname + " " + item.Name;
+                result.count = list_vouch.Count;
+                result.money = Convert.ToDecimal(money);
+                allTables.Add(result);
+            }
+            return View(allTables);
+        }
+    }
+}
---
+++ Lab1/Models/DetailModels/DetailEmployee.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using Lab1.Models.Entities;
+
+namespace Lab1.Models.DetailModels
+{
+    public class DetailEmployee
+    {
+        public string bio { get; set; }
+
+        public int count { get; set; }
+
+        public decimal money { get; set; }
+    }
+}
